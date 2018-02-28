52eebd1845f780631626b01b2dbcdfdfe9e01b9a
devashree1639 <36419883+devashree1639@users.noreply.github.com>
2018-02-28 16:19:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('hello.html',function(req,res)
+app.get('/ui/hello.html',function(req,res)
 {
  res.send('Hi');   
 });
